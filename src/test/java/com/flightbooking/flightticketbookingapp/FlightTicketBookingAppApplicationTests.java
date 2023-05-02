2f6491b977d35a2316680bf791d63bc7f561d0da
nama.gupta <nama.gupta@globallogic.com>
2023-05-02 18:38:58 +0530

    Project setup.

---
+++ src/test/java/com/flightbooking/flightticketbookingapp/FlightTicketBookingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flightbooking.flightticketbookingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightTicketBookingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
